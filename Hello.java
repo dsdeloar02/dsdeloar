e52ad3f770cc004bb54f7e40c4a420a7b4347b17
dsdeloar02 <dsdeloar02@gmail.com>
2021-11-06 21:05:34 +0600

    Get new code for show method of java Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
     public static void main(String[] args){
         System.out.printIn("Vs code is Awsome..");
+
+        greeting("Deluar")
+    }
+    public static void greeting(String name){
+        System.out.printIn("Welcome" + name);
     }
 }
